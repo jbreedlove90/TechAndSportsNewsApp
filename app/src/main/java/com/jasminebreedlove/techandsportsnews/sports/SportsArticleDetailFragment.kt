4b89af30e8b6c2a4f155e467f0ef220c1cf21333
Jasmine Breedlove <jbreedlove11@gmail.com>
2018-05-16 19:16:31 -0500

    added link to full article in article detail activities

--- app/src/main/java/com/jasminebreedlove/techandsportsnews/sports/SportsArticleDetailFragment.kt
+++ app/src/main/java/com/jasminebreedlove/techandsportsnews/sports/SportsArticleDetailFragment.kt
@@ -2,9 +2,14 @@ package com.jasminebreedlove.techandsportsnews.sports
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.URLSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.jasminebreedlove.techandsportsnews.R
 import com.jasminebreedlove.techandsportsnews.dao.Article
 import kotlinx.android.synthetic.main.activity_sportsarticle_detail.*
@@ -42,6 +47,14 @@ class SportsArticleDetailFragment : Fragment() {
             activity?.sports_toolbar_layout?.title = it.articleTitle
             rootView.sports_article_pub_date.text = it.pubDate
             rootView.sports_article_description.text = it.description
+            rootView.sports_article_link.apply {
+                val spanStringBuilder = SpannableStringBuilder().apply {
+                    append(resources.getString(R.string.full_story_text))
+                    setSpan(URLSpan(it.link), 0, this.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                }
+                setText(spanStringBuilder, TextView.BufferType.SPANNABLE)
+                movementMethod = LinkMovementMethod.getInstance()
+            }
         }
         return rootView
     }
--- app/src/main/java/com/jasminebreedlove/techandsportsnews/tech/TechArticleDetailFragment.kt
+++ app/src/main/java/com/jasminebreedlove/techandsportsnews/tech/TechArticleDetailFragment.kt
@@ -1,10 +1,19 @@
 package com.jasminebreedlove.techandsportsnews.tech
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.text.Html
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.URLSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.jasminebreedlove.techandsportsnews.R
 import com.jasminebreedlove.techandsportsnews.dao.Article
 import kotlinx.android.synthetic.main.activity_article_detail.*
@@ -39,9 +48,17 @@ class TechArticleDetailFragment : Fragment() {
         // todo: add category to view and xml layout file
         // load article details in respective views
         article.let {
-            activity?.toolbar_layout?.title = it.articleTitle
+            activity?.toolbar_layout?.title = it.articleTitle // set toolbar title to article title
             rootView.article_pub_date.text = it.pubDate
             rootView.article_description.text = it.description
+            rootView.article_link.apply {
+                val spanStringBuilder = SpannableStringBuilder().apply {
+                    append(resources.getString(R.string.full_story_text))
+                    setSpan(URLSpan(it.link), 0, this.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                }
+                setText(spanStringBuilder, TextView.BufferType.SPANNABLE)
+                movementMethod = LinkMovementMethod.getInstance()
+            }
         }
         return rootView
     }
