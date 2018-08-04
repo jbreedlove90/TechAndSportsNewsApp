afe0847848a4a979fb93def6f0753ce0529af50f
Jasmine Breedlove <jbreedlove11@gmail.com>
2018-08-03 22:56:32 -0500

    made some changes to tech design and api changes

---
+++ app/src/main/java/com/jasminebreedlove/techandsportsnews/api/NewsWebService.kt
@@ -0,0 +1,11 @@
+package com.jasminebreedlove.techandsportsnews.api
+
+/**
+ * Created by Jasmine Breedlove on 8/3/2018
+ *
+ * Copyright (c) 2018 OldMoonStudios. All rights reserved.
+ *
+ * http://www.oldmoonstudios.com/
+ */
+class NewsWebService {
+}
