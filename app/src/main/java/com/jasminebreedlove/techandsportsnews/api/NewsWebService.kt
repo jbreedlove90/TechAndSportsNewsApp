76c7f6c07a4ab4e83182c5ee3b2a43ca465a4719
Jasmine Breedlove <jbreedlove11@gmail.com>
2022-05-13 22:34:16 -0500

    remove oms text

--- app/src/main/java/com/jasminebreedlove/techandsportsnews/api/NewsWebService.kt
+++ app/src/main/java/com/jasminebreedlove/techandsportsnews/api/NewsWebService.kt
@@ -3,9 +3,8 @@ package com.jasminebreedlove.techandsportsnews.api
 /**
  * Created by Jasmine Breedlove on 8/3/2018
  *
- * Copyright (c) 2018 OldMoonStudios. All rights reserved.
+ * Copyright (c) 2018 Jasmine Breedlove. All rights reserved.
  *
- * http://www.oldmoonstudios.com/
  */
 class NewsWebService {
 }
